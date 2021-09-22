6592bc2b195af91fb80f4e1cac4e9a7a6af2db51
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-22 14:49:29 -0500

    commit tree e0952ebadaa4adf1d1489aa00bc5706785b5247c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// test comment 
+const num1 = 31 
+const num2 = 2
+const multiply = num1 * num2
+// First test wants us to create a vairable that multiplies num1 & num2 to equal 62 
+const random = Math.floor(Math.random() * 10)
+// Second test wants us to create a variable called random that generates a number greater than 1
+const num3 = 8
+const num4 = 2
+const mod = num3 / num4
+// Third test wants ust to create a variable mod that is equal to num3 divided by num4 and returns 4
+const max = Math.max(5,10,15,20)
